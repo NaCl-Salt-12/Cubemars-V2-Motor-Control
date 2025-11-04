dce9c6e3bc9ae626fd74bdd5f0a13f67149fe695
NaCl-Salt-12 <cla21052@byui.edu>
2025-11-04 12:49:09 -0500

    added recording node to save motor topics to csvs

---
+++ src/cubemars_v2_ros/cubemars_v2_ros/record_motor.py
@@ -0,0 +1,128 @@
+import rcply
+from rclpy.node import Node
+import csv
+import datetime
+import os
+from pathlib import Path
+
+from std_msgs.msg import Float64MultiArray, String
+from motor_interfaces.msg import MotorState
+
+
+class RecordMotor(Node):
+    def __init__(self):
+        super().__init__("record_motor")
+
+        (self.declare_parameter("experiment_name", ""),)
+        self.declare_parameter("motor_name")
+        self.declare_parameter("save_to", "./")
+
+        self.motor_name = (
+            self.get_parameter("motor_name").get_parameter_value().string_value
+        )
+        self.save_to = self.get_parameter("save_to").get_parameter_value().string_value
+        self.experiment_name = (
+            self.get_parameter("experiment_name").get_parameter_value().string_value
+        )
+
+        parent_folder = Path(self.save_to)
+        # change to date_experiment_name format?
+        save_folder = parent_folder / self.experiment_name / self.motor_name
+        save_folder.mkdir(parents=True, exist_ok=True)
+
+        mit_csv_path = (
+            save_folder / f"{self.experiment_name}_{self.motor_name}_mit_cmd.csv"
+        )
+        error_csv_path = (
+            save_folder / f"{self.experiment_name}_{self.motor_name}_error_code.csv"
+        )
+        motor_state_csv_path = (
+            save_folder / f"{self.experiment_name}_{self.motor_name}_motor_state.csv"
+        )
+
+        self.mit_file = open(mit_csv_path, "w", newline="")
+        self.error_file = open(error_csv_path, "w", newline="")
+        self.state_file = open(motor_state_csv_path, "w", newline="")
+
+        self.mit_writer = csv.writer(self.mit_file)
+        self.error_writer = csv.writer(self.error_file)
+        self.state_writer = csv.writer(self.state_file)
+
+        self.mit_writer.writerow(
+            ["timestamp", "position_cmd", "velocity_cmd", "kp", "kd", "torque_cmd"]
+        )
+        self.error_writer.writerow(["timestamp", "error_code"])
+        self.state_writer.writerow(
+            [
+                "timestamp",
+                "name",
+                "position",
+                "abs_position",
+                "velocity",
+                "torque",
+                "current",
+                "temperature",
+            ]
+        )
+
+        self.state_sub = self.create_subscription(
+            MotorState, "/self.motor_name/motor_state", self.state_callback, 10
+        )
+
+        self.error_sub = self.create_subscription(
+            String, f"/{self.motor_name}/error_code", self.error_callback, 10
+        )
+
+        self.mit_sub = self.create_subscription(
+            Float64MultiArray, "/{self.motor_name}/mit_cmd", self.mit_callback, 10
+        )
+
+    def mit_callback(self, msg):
+        timestamp = self.get_clock().now().nanoseconds / 1e-9
+        self.mit_writer.writerow([timestamp] + list(msg.data))
+        self.mit_file.flush()
+
+    def error_callback(self, msg):
+        timestamp = self.get_clock().now().nanoseconds / 1e-9
+        self.error_writer.writerow([timestamp, msg.data])
+        self.error_file.flush()
+
+    def state_callback(self, msg):
+        timestamp = self.get_clock().now().nanoseconds / 1e-9
+        self.state_writer.writerow(
+            [
+                timestamp,
+                msg.name,
+                msg.position,
+                msg.abs_position,
+                msg.velocity,
+                msg.torque,
+                msg.current,
+                msg.temperature,
+            ]
+        )
+        self.state_file.flush()
+
+    def __del__(self):
+        self.mit_file.close()
+        self.error_file.close()
+        self.state_file.close()
+
+
+def main():
+    rclpy.init()
+    node = RecordMotor()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        self.mit_file.close()
+        self.error_file.close()
+        self.state_file.close()
+        node.destroy_node()
+        rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
--- src/cubemars_v2_ros/setup.py
+++ src/cubemars_v2_ros/setup.py
@@ -1,27 +1,27 @@
 from setuptools import find_packages, setup
 
-package_name = 'cubemars_v2_ros'
+package_name = "cubemars_v2_ros"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='ciscor',
-    maintainer_email='ciscor@todo.todo',
-    description='Cubemars AK70/AK80 joystick control nodes (ROS 2)',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="ciscor",
+    maintainer_email="ciscor@todo.todo",
+    description="Cubemars AK70/AK80 joystick control nodes (ROS 2)",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
+        "console_scripts": [
             # existing nodes
-            'motor_node = cubemars_v2_ros.motor_node:main'
+            "motor_node = cubemars_v2_ros.motor_node:main"
+            "record_node = cubemars_v2_ros.record_node:main",
         ],
     },
 )
